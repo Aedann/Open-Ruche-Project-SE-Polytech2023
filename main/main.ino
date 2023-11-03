33747bb96dded209975b5ab482c9d664a1817fd4
Nathan SEIGNOLE <nseignole@gmail.com>
2023-11-03 08:14:01 +0100

    added onewire probes and battery charge to main packet. untested.

--- main/main.ino
+++ main/main.ino
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 #include <MKRWAN.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 #include "HX711.h"
 #include "DHT11.h"
 
@@ -11,17 +13,21 @@
 
 const int DHT_PIN = 7;
 DHT11 dht(DHT_PIN);
-short inTemp;
-short inHum;
+
+// OneWire Temp Probe-related
+const int ONEWIRE_PIN_A = 0;
+const int ONEWIRE_PIN_B = 1;
+OneWire oneWire_a(ONEWIRE_PIN_A);
+OneWire oneWire_b(ONEWIRE_PIN_B);
+DallasTemperature probe_a(&oneWire_a);
+DallasTemperature probe_b(&oneWire_b);
 
 // Scale-related
-
 #define SCALE_MEANSF 286.53
 #define SCALE_METALZERO 1258.7
 const int LOADCELL_DOUT_PIN = 2;
 const int LOADCELL_SCK_PIN = 3;
 HX711 scale;
-short HiveWeight;
 
 // Battery-related
 #define MAXVOLTAGE 3.3
@@ -29,7 +35,6 @@ short HiveWeight;
 const int BATTERY_PIN = A0;
 
 // WAN-related 
-
 const String appEui = "A8610A34353B6010";
 const String appKey = "C1868E3C1B819DADBE3A8AEB2711949A";
 LoRaModem modem;
@@ -37,8 +42,7 @@ int err_count= 0;
 short con = 0;
 
 // Buzzer-related
-
-const int BUZZER_PIN = 2;
+const int BUZZER_PIN = 10;
 
 enum t_STATE {IDLE, CONNECTING, ERR, MEASURE, SENDING};
 t_STATE State;
@@ -49,6 +53,11 @@ void init_serial() {
 
 void init_i2s() {
 
+}
+
+void init_OneWire() {
+  probe_a.begin();
+  probe_b.begin();
 }
 
 void init_HX711() {
@@ -62,11 +71,14 @@ void init_WAN() {
 
 void beep() {
   digitalWrite(BUZZER_PIN,HIGH);
+  delay(1000);
+  digitalWrite(BUZZER_PIN,LOW);
 }
 
 void setup() {
   init_serial();
   init_i2s();
+  init_OneWire();
   init_HX711();
   init_WAN();
   beep();
@@ -75,6 +87,12 @@ void setup() {
 }
 
 void loop() {
+  short inTemp;
+  short inHum;
+  short tempA;
+  short tempB;
+  short HiveWeight;
+  short batteryCharge;
   switch(State) {
     case CONNECTING:
     {
@@ -94,11 +112,20 @@ void loop() {
 
     case MEASURE:
     {
+      // DHT Measures
       inTemp = ((float)dht.readTemperature())*10;
       inHum = ((float)dht.readHumidity())*10;
+      // OneWire Measures
+      probe_a.requestTemperatures();
+      probe_b.requestTemperatures();
+      tempA = ((float)probe_a.getTempCByIndex(0)*10);
+      tempB = ((float)probe_b.getTempCByIndex(0)*10);
+      // Scale Measures
       HiveWeight = (scale.get_units(10) - SCALE_METALZERO);
-      Serial.print("Weight:\t");
-      Serial.println(HiveWeight);
+      // Battery Measures
+      float batteryTemp = analogRead(BATTERY_PIN);
+      batteryTemp = (batteryTemp / 1023.0) * 3.3;
+      batteryCharge = (float)((batteryTemp - MINVOLTAGE)/(MAXVOLTAGE - MINVOLTAGE) * 100);
       State = SENDING;
       break;
     }
@@ -109,7 +136,10 @@ void loop() {
       modem.beginPacket();
       modem.write(inHum);
       modem.write(inTemp);
-      modem.write(HiveWeight);  
+      modem.write(tempA);
+      modem.write(tempB);
+      modem.write(HiveWeight);
+      modem.write(batteryCharge);  
       err = modem.endPacket();
       if ( err <= 0 ) {
         Serial.print("Error : ");
